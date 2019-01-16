d12e3fda17284cb24eaa2e9dc9e3e992ff2e770d
TR45HM4N1721 <34109430+TR45HM4N1721@users.noreply.github.com>
2019-01-16 13:20:42 -0500

    more documentation

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -14,7 +14,7 @@ public class DriveTrain extends Subsystem {
   public void initDefaultCommand() {
     // setDefaultCommand(new MySpecialCommand());
   }
-
+  /** Drive using two TalonSRX and a joystick */
   public static void flyByWire(TalonSRX starboard, TalonSRX port, Joystick DriverJoystick){
     double thro = DriverJoystick.getRawAxis(1); //Populate the double thro with the raw axis 1
     double yaw = DriverJoystick.getRawAxis(2); //Populate the double yaw with the raw axis 2
@@ -22,7 +22,7 @@ public class DriveTrain extends Subsystem {
     starboard.set(ControlMode.PercentOutput, (-1 * thro) + yaw); //Subtract the steerage for arcade drive
     port.set(ControlMode.PercentOutput, thro + yaw); //Subtract the steerage for arcade drive, reverse
   }
-  
+  /** Drive using two TalonSRX and adjustment values for horizontal and distance correction */
   public static void flyWithWires(TalonSRX starboard, TalonSRX port, float heading, float throttle){
     starboard.set(ControlMode.PercentOutput, heading + throttle); //Set the starboard drivetrain motor to the heading (steering angle) added to the base speed
     port.set(ControlMode.PercentOutput, heading - throttle); //Does the same but on the other side
